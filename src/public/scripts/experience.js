fac0428dc0765711fa7c26c98c3acb9978bbd1ae
Antony <86053464+AntonyLoose@users.noreply.github.com>
2024-09-03 20:35:24 +1000

    refactor: made references clearer

--- src/public/scripts/experience.js
+++ src/public/scripts/experience.js
@@ -58,27 +58,30 @@ function generate_star_and_add_to_dom() {
 }
 
 // EMAILS
-function show_references() {
-	const auslan_ref = document.getElementById("auslan-ref");
+const auslan_ref = document.getElementById("auslan-ref");
+auslan_ref.onclick = () => {
 	const auslan_text_content = "ka<span class='spam-protection'>Seriously, the fact that I need to do this says a lot about the world</span>lin.stefan<!-- 123stopspam -->ov@mona<!-- 123stopspam -->sh.edu ";
 	auslan_ref.innerHTML = auslan_text_content;
 	auslan_ref.className = "subscript marginless";
+}
 
-	const ilearn_ref = document.getElementById("ilearn-ref");
+const ilearn_ref = document.getElementById("ilearn-ref");
+ilearn_ref.onclick = () => {
 	const ilearn_text_content = "devmym<!-- 123stopspam -->ai<span class='spam-protection'>No, go away</span>l1@gm<!-- 123stopspam -->ail.com";
 	ilearn_ref.innerHTML = ilearn_text_content;
 	ilearn_ref.className = "subscript marginless";
+}
 
-	const intake_ref = document.getElementById("intake-ref");
+const intake_ref = document.getElementById("intake-ref");
+intake_ref.onclick = () => {
 	const intake_text_conent = "nabeeb2<!-- 123stopspam -->000@g<span class='spam-protection'>Nice try buddy</span>mai<!-- 123stopspam -->l.com";
 	intake_ref.innerHTML = intake_text_conent;
 	intake_ref.className = "subscript marginless";
+}
 
-	const woolies_ref = document.getElementById("woolies-ref");
+const woolies_ref = document.getElementById("woolies-ref");
+woolies_ref.onclick = () => {
 	const woolies_text_content = "042<!-- 123stopspam -->6 16<span class='spam-protection'>No, I don't want you messaging my ex-manager saying this website is out of date, thanks and goodbye.</span>9 41<!-- 123stopspam -->7";
 	woolies_ref.innerHTML = woolies_text_content;
 	woolies_ref.className = "subscript marginless";
 }
-
-const show_refs_button = document.getElementById("show-references");
-show_refs_button.onclick = show_references;
