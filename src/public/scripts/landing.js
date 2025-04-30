a823183337f6d616a1fa36fb24fc745e70cb79fd
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-04-30 14:10:56 +1000

    clean: run prettier

--- src/public/scripts/landing.js
+++ src/public/scripts/landing.js
@@ -1,32 +1,47 @@
 import { fade_element_in, fade_element_out } from "./index.js";
 
 const landing_background_images = [
-	"/images/melbourne-night.jpg",
-	"/images/woodside.jpg",
-	"/images/murray-river-night.jpeg",
-]
+  "/images/melbourne-night.jpg",
+  "/images/woodside.jpg",
+  "/images/murray-river-night.jpeg",
+];
 
 function swap_current_image() {
-	const container = document.getElementById("background-container");
-	const curr_background_img = document.getElementById("background");
-	const id = curr_background_img.src;
-	const curr_background_index = landing_background_images.findIndex(img_src => id.includes(img_src.slice(2)));
-	const next_background_index = curr_background_index + 1 >= landing_background_images.length ? 0 : curr_background_index + 1;
-	const new_background_img = document.createElement("img");
-	curr_background_img.style.position = "absoulte";
-	new_background_img.src = landing_background_images[next_background_index];
-	new_background_img.className = "bg-img";
-	new_background_img.style.opacity = "0";
-	new_background_img.style.position = "absolute";
-	container.appendChild(new_background_img);
+  const container = document.getElementById("background-container");
+  const curr_background_img = document.getElementById("background");
+  const id = curr_background_img.src;
+  const curr_background_index = landing_background_images.findIndex((img_src) =>
+    id.includes(img_src.slice(2)),
+  );
+  const next_background_index =
+    curr_background_index + 1 >= landing_background_images.length
+      ? 0
+      : curr_background_index + 1;
+  const new_background_img = document.createElement("img");
+  curr_background_img.style.position = "absoulte";
+  new_background_img.src = landing_background_images[next_background_index];
+  new_background_img.className = "bg-img";
+  new_background_img.style.opacity = "0";
+  new_background_img.style.position = "absolute";
+  container.appendChild(new_background_img);
 
-	fade_element_out(curr_background_img, () => {
-		container.removeChild(curr_background_img);
-	}, 10, 0.005);
-	fade_element_in(new_background_img, () => {
-		new_background_img.id = "background";
-		setTimeout(swap_current_image, 5000);
-	}, 10, 0.005);
+  fade_element_out(
+    curr_background_img,
+    () => {
+      container.removeChild(curr_background_img);
+    },
+    10,
+    0.005,
+  );
+  fade_element_in(
+    new_background_img,
+    () => {
+      new_background_img.id = "background";
+      setTimeout(swap_current_image, 5000);
+    },
+    10,
+    0.005,
+  );
 }
 
 setTimeout(swap_current_image, 5000);
--- src/server.js
+++ src/server.js
@@ -7,29 +7,29 @@ app.use(express.static("public"));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
-	res.status(200);
-	res.set("Content-Type", "text/html");
-	res.sendFile(path.join(__dirname, "/pages/landing.html"));
-})
+  res.status(200);
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/pages/landing.html"));
+});
 
 app.get("/about-me", (_, res) => {
-	res.status(200);
-	res.set("Content-Type", "text/html");
-	res.sendFile(path.join(__dirname, "/pages/about-me.html"));
-})
+  res.status(200);
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/pages/about-me.html"));
+});
 
 app.get("/experience", (_, res) => {
-	res.status(200);
-	res.set("Content-Type", "text/html");
-	res.sendFile(path.join(__dirname, "/pages/experience.html"));
-})
+  res.status(200);
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/pages/experience.html"));
+});
 
 app.get("/education", (_, res) => {
-	res.status(200);
-	res.set("Content-Type", "text/html");
-	res.sendFile(path.join(__dirname, "/pages/education.html"));
-})
+  res.status(200);
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "/pages/education.html"));
+});
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port: ${PORT}`);
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
